e6110a4d3c0b617a92d17aafb837133609e25874
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:09:32 +0530

    Update use-counter.ts

--- src/components/hooks/use-counter.ts
+++ src/components/hooks/use-counter.ts
@@ -1,18 +1,24 @@
 /* node modules */
 import { useState } from "react";
 
-/* hook */
+/**
+  * @returns - value of the counter, along with functionalities for updating/mutating the value of the counter
+  * as per user interactions.
+  */
 function useCounterHook() {
   const [counter, setCounter] = useState<number>(0);
 
+  /* increments value of counter by 1 */
   function counterIncrement() {
     setCounter((prevState) =>  prevState + 1);
   }
 
+  /* decrements value of counter by 1 */
   function counterDecrement() {
     setCounter((prevState) =>  prevState - 1);
   }
 
+  /* resets the counter value to zero */
   function counterReset() {
     setCounter(() => 0);
   }
