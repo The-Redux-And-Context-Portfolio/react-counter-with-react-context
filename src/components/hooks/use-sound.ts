8db112ca28ea251dc4e430f3f11e07194f7b4816
Sricharan Krishnan <sricharan.krishnan@gmail.com>
2024-05-15 09:11:45 +0530

    Update use-sound.ts

--- src/components/hooks/use-sound.ts
+++ src/components/hooks/use-sound.ts
@@ -1,14 +1,19 @@
 /* node modules */
 import { useState } from "react";
 
-/* hook */
+/**
+  * @returns - the 'sound' variable that determines if sound can be played in the app as well as functionalities 
+  * that help control the state.
+  */
 function useSoundHook() {
   const [sound, setSound] = useState<boolean>(true);
 
+  /* activates the sound in the app */
   function soundOn() {
     setSound(() => true);
   }
 
+  /* deactivates the sound in the app */
   function soundOff() {
     setSound(() =>  false);
   }
